a0c34a195a7935ceec5c6375093c05ca091963a7
ChiHong Lin <chihong.lin@gmail.com>
2014-10-29 00:58:42 +0800

    API Ver.1

--- routes/index.js
+++ routes/index.js
@@ -17,4 +17,68 @@ router.get('/ocr', function(req, res) {
   	res.render('ocrtext', { title: 'OCR', ocrtext:tesstext });
 });
 
+/* GET home page. */
+router.post('/ocr', function(req, res) {
+	console.log('Got it');
+
+	var formidable = require('formidable')
+	var path = require('path')
+	var dv = require('dv');
+	var fs = require('fs');
+	var form = new formidable.IncomingForm();
+	form.parse(req, function(err, fields, files) {
+
+		console.log(files.myfile.path+'/'+files.myfile.name);
+        
+        var langcode = fields.langcode
+        console.log(langcode)
+
+		//Assign File Type parameter 
+		var imagetype='png'
+		var imagefilename = new Date().getTime()
+		var tempPath = files.myfile.path
+		var targetPath = './tmpimages/'+imagefilename+'.png';
+		switch(files.myfile.type) {
+			case 'image/jpeg':
+				imagetype='jpg'
+				targetPath = './tmpimages/'+imagefilename+'.jpg'
+				break;
+			case 'image/gif':
+				imagetype='gif'
+				targetPath = './tmpimages/'+imagefilename+'.gif'
+				break;
+			default:
+				imagetype='png'
+				targetPath = './tmpimages/'+imagefilename+'.png'
+		}
+
+		//Save Uploaded file to temp folder 
+		fs.rename(tempPath, path.resolve(targetPath), function(err) {
+            if (err) throw err;
+            console.log("Upload completed!");
+
+            //Handle OCR Processing
+    	  	var image = new dv.Image(imagetype, fs.readFileSync(targetPath));
+			var tesseract = new dv.Tesseract(langcode, image);
+			var tesstext = tesseract.findText('plain')
+			console.log(tesstext);
+
+			//Make Response Datas
+			res.send({ocrtext:tesstext,ocrfile:targetPath,ocrlang:langcode})
+		  	// res.render('ocrtext', { title: 'OCR', ocrtext:tesstext ,ocrfile:'./tmpimages/image.jpg'});
+
+        });
+
+		
+
+
+
+    });
+
+});
+
+router.get('/upload', function(req, res) {
+  	res.render('upload', { title: 'Upload' });
+});
+
 module.exports = router;
