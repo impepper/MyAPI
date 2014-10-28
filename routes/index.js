accea324acaeb12d2e7bf787b2642ed661979c63
ChiHong Lin <chihong.lin@gmail.com>
2014-10-28 15:53:16 +0800

    DV local

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,13 @@ router.get('/', function(req, res) {
 
 /* GET home page. */
 router.get('/ocr', function(req, res) {
-  res.render('index', { title: 'OCR' });
+	var dv = require('dv');
+	var fs = require('fs');
+	var image = new dv.Image('png', fs.readFileSync('textpage300.png'));
+	var tesseract = new dv.Tesseract('eng', image);
+	var tesstext = tesseract.findText('plain')
+	// console.log(tesstext);
+  	res.render('ocrtext', { title: 'OCR', ocrtext:tesstext });
 });
 
 module.exports = router;
